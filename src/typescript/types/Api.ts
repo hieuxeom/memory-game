c99560f1b12933fd08c0045535ef056e76d63169
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-04-06 02:04:24 +0700

    chore: add some API's interface
    - IApiResponse
    - ICardTheme
    - IGameTopic

---
+++ src/typescript/types/Api.ts
@@ -0,0 +1,49 @@
+export enum FetchStatus {
+    SUCCESS = "success",
+    FAILURE = "failure",
+    ERROR = "error",
+    REDIRECT = "redirect"
+}
+
+export type ResponseStatus = "success" | "failure" | "error" | "redirect"
+
+export type ErrorDetails = {
+    name: string,
+    message: string
+}
+export interface IApiResponse {
+    status: ResponseStatus,
+    message: string,
+    data?: any;
+    url?: string;
+    error?: ErrorDetails;
+}
+
+export interface ICardTheme {
+    _id: string;
+    themeName: string;
+    cardBack: string;
+    cardFront: string;
+    price: number;
+    used: number;
+    isVip: boolean;
+    isDeleted: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface IGameTopic {
+    _id: string;
+    themeName: string;
+    themeData: any;
+    rawData: string;
+    themeThumbnail: string;
+    type: string;
+    isVip: boolean;
+    price: number;
+    isDeleted: boolean;
+    played: number;
+    createAt: string;
+    updatedAt: string;
+
+}
