324b5cc0af1c96df0d0b6bf757f27cac714ace9e
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-04-11 12:27:48 +0700

    feat: write readme.md file

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,19 +1,19 @@
 class MongoDB {
-    constructor() {
-        this.mongoose = require("mongoose");
-        this.connectString = process.env.MONGO_CONNECT_STRING
-        console.log(process.env.MONGO_CONNECT_STRING)
-    }
+	constructor() {
+		this.mongoose = require("mongoose");
+		this.connectString = process.env.MONGO_CONNECT_STRING;
+		console.log(process.env.MONGO_CONNECT_STRING);
+	}
 
-    async connect() {
-        try {
-            await this.mongoose.connect(this.connectString);
-            return console.log("Connect MongoDB successfully");
-        } catch (err) {
-            console.log(err.name, " - ", err.message);
-            return console.log("Error connecting to MongoDB")
-        }
-    }
+	async connect() {
+		try {
+			await this.mongoose.connect(this.connectString);
+			return console.log("Connect MongoDB successfully");
+		} catch (err) {
+			console.log(err.name, " - ", err.message);
+			return console.log("Error connecting to MongoDB");
+		}
+	}
 }
 
 module.exports = new MongoDB();
